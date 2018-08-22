f436a646ea80940f3fc1078c16076ef65fc63b97
dan <daniel.mamaid@hpe.com>
2018-08-22 02:58:27 -0700

    to commit only

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -2,7 +2,13 @@
       <div class="card card-login mx-auto mt-5">
         <div class="card-header">Login</div>
         <div class="card-body">
-          <form>
+        <form class="form-signin"  method="post" accept-charset="utf-8" action="<?php echo base_url(); ?>login/validate_account">
+          <?php if(validation_errors() == FALSE){
+								echo validation_errors(); 
+								}
+			  					else{ ?>	
+				  					<div class="alert alert-danger" role="alert"style="margin-top:5px"><?php echo validation_errors(); ?></div>
+			  					<?php  } ?>
             <div class="form-group">
               <div class="form-label-group">
                 <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus">
@@ -23,7 +29,7 @@
                 </label>
               </div>
             </div>
-            <a class="btn btn-primary btn-block" href="index.html">Login</a>
+            <input type="submit" class="btn btn-primary btn-block" value="Login">
           </form>
           <div class="text-center">
             <a class="d-block small mt-3" href="register.html">Register an Account</a>
